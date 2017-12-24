afbce7bc84743d0c6af53f346ed18df7d0617175
Mario Galic <mario.galic@guardian.co.uk>
2017-12-24 13:40:39 +0000

    Initial commit

--- src/main/scala/com/gu/tip/Notifier.scala
+++ src/main/scala/com/gu/tip/Notifier.scala
@@ -1,14 +1,16 @@
 package com.gu.tip
 
+import cats.data.WriterT
 import cats.effect.IO
 import com.typesafe.scalalogging.LazyLogging
+import cats.implicits._
 
 trait NotifierIf { this: GitHubApiIf =>
-  def setLabelOnLatestMergedPr(): IO[String]
+  def setLabelOnLatestMergedPr(): WriterT[IO, String, String]
 }
 
 trait Notifier extends NotifierIf with LazyLogging { this: GitHubApiIf =>
-  def setLabelOnLatestMergedPr(): IO[String] =
+  def setLabelOnLatestMergedPr(): WriterT[IO, String, String] =
     for {
       prNumber <- getLastMergedPullRequestNumber()
       responseBody <- setGitHubLabel(prNumber)
@@ -22,16 +24,27 @@ trait Notifier extends NotifierIf with LazyLogging { this: GitHubApiIf =>
 
     So we try to pick out pull request number #118
   */
-  private def getLastMergedPullRequestNumber(): IO[String] = {
-    getLastMergeCommitMessage().map { message =>
-      val prNumberPattern = """#\d+""".r
-      prNumberPattern.findFirstIn(message).get.tail // Using get() because currently we just swallow any exception in Tip.verify()
+  private def getLastMergedPullRequestNumber(): WriterT[IO, String, String] = {
+    WriterT {
+      getLastMergeCommitMessage().map { message =>
+        val prNumberPattern = """#\d+""".r
+        val prNumber = prNumberPattern.findFirstIn(message).get.tail // Using get() because currently we just swallow any exception in Tip.verify()
+        (
+          s"Successfully retrieved PR number of the last merged PR: $prNumber",
+          prNumber
+        )
+      }
+
     }
   }
 
-  private def setGitHubLabel(prNumber: String): IO[String] =
-    setLabel(prNumber).map { response =>
-      logger.info(s"Successfully set verification label on PR $prNumber")
-      response
+  private def setGitHubLabel(prNumber: String): WriterT[IO, String, String] =
+    WriterT {
+      setLabel(prNumber).map { response =>
+        (
+          s"Successfully set verification label on PR $prNumber",
+          response
+        )
+      }
     }
 }
